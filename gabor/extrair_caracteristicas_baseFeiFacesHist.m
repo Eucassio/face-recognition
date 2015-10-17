73face02279e36f89e3e01a46ecb01ec478d0fbb
Eucassio Gonçalves Lima Júnior <eucassiojr@gmail.com>
2015-10-17 19:04:22 -0300

    adicionando histograma

---
+++ gabor/extrair_caracteristicas_baseFeiFacesHist.m
@@ -0,0 +1,89 @@
+%%
+% Procedure that extracts the vector of characteristics of an image database using the entropy of the Gabor
+%%
+
+% image directory
+database  = 'CroppedYale_crop';
+caminho = ['/home/eucassio/projetos/mestrado/' database];
+fileFolder = fullfile(caminho);
+dirOutput = dir(fullfile(fileFolder,'*.pgm'));
+fileNames = {dirOutput.name}';
+numFrames = numel(fileNames);
+cont = 1;
+
+% vector with sizes of windows entropy
+blocks = [6];
+[tamx, tamy] = size(blocks);
+numFacesDif = [1,3,10,64];
+[tamxNumFacesDif, tamyNumFacesDif] = size(numFacesDif);
+t = 1;
+for a = 1:int64(tamy)
+    for r = 1:int64(tamyNumFacesDif)
+        numFacesDiff = numFacesDif(r);
+        
+    disp(['Starting ', int2str(a) ,' in ', datestr(now, 'HH:MM:SS')]);
+    block_size = blocks(a);
+     name = [fileFolder '/' fileNames{1}];
+     img = imread(name);
+    % [img_h, img_v] = lvp_lee(img, block_size);
+    
+    %variables to downsample    
+    d1 = 1;
+    d2 = 1;
+    
+    gaborArray = gaborFilterBank(1,4,9,9);
+    [u,v] = size(gaborArray);
+     [n,m] = size(img);
+     s = (n*m)/(d1*d2);
+     l = s/(block_size*block_size);
+    
+    people_name = '';
+    i = 0;
+    m = 10;
+    training_label_vector = zeros(m,1);
+    training_instance_matrix = zeros(m,l);
+    k = 1;
+
+    z = 1;
+    imgEquals = 1;
+    while(k <= numFrames)
+        name = [fileFolder '/' fileNames{k}];
+
+            [people_name_tmp, imgIdx] = strtok(fileNames{k},'_');
+            %[people_name_tmp, imgIdx] = strtok(imgIdx,'-');
+            %if(strcmp(imgIdx,'-11.jpg') == 1 || strcmp(imgIdx,'-13.jpg') == 1 || strcmp(imgIdx,'-11 1.jpg') == 1)
+                if(strcmp(people_name,people_name_tmp) == 0)
+                    people_name = people_name_tmp;
+                    i = i + 1;
+                    imgEquals = 1;
+                else
+                    imgEquals = imgEquals + 1;
+                end
+                if(imgEquals <= numFacesDiff)
+                    disp([int2str(i) ' - ' int2str(imgEquals) ' - ' name]);
+                    img = imread(name);
+
+                    %featureVector_h = gaborFeatures2(img, gaborArray,d1,d2,s,l,block_size);
+                    %featureVector_h = gaborFeatures(img, gaborArray,d1,d2);
+                    featureVector_h = gaborFeaturesHist(img, gaborArray,d1,d2,block_size,block_size);
+
+                    training_label_vector(z) = i;
+                    training_instance_matrix(z,1:l) = featureVector_h';
+
+                    z = z + 1;
+                    numInd = imgEquals;
+                end
+            %end
+            k = k + 1;            
+    end;
+    nameOutput = strcat('hist_', database, '_',int2str(i), '_', int2str(numInd), '_' , int2str(block_size),'x',int2str(block_size),'.txt');
+    disp('Saving ...');
+    disp(nameOutput);
+    libsvmwrite(nameOutput, training_label_vector, sparse(training_instance_matrix));
+    disp(['End ', int2str(a) ,' in ', datestr(now, 'HH:MM:SS')]);
+    end
+    
+    %%-------------------------------------------------------------
+    
+end
+disp('Over all');
---
+++ gabor/gaborFeaturesHist.m
@@ -0,0 +1,169 @@
+function featureVector = gaborFeaturesHist(img,gaborArray,d1,d2, N, K)
+
+% GABORFEATURES extracts the Gabor features of the image.
+% It creates a column vector, consisting of the image's Gabor features.
+% The feature vectors are normalized to zero mean and unit variance.
+%
+%
+% Inputs:
+%       img         :	Matrix of the input image 
+%       gaborArray	:	Gabor filters bank created by the function gaborFilterBank
+%       d1          :	The factor of downsampling along rows.
+%                       d1 must be a factor of n if n is the number of rows in img.
+%       d2          :	The factor of downsampling along columns.
+%                       d2 must be a factor of m if m is the number of columns in img.
+%               
+% Output:
+%       featureVector	:   A column vector with length (m*n*u*v)/(d1*d2). 
+%                           This vector is the Gabor feature vector of an 
+%                           m by n image. u is the number of scales and
+%                           v is the number of orientations in 'gaborArray'.
+%
+%
+% Sample use:
+% 
+% img = imread('cameraman.tif');
+% gaborArray = gaborFilterBank(5,8,39,39);  % Generates the Gabor filter bank
+% featureVector = gaborFeatures(img,gaborArray,4,4);   % Extracts Gabor feature vector, 'featureVector', from the image, 'img'.
+% 
+% 
+%   Details can be found in:
+%   
+%   M. Haghighat, S. Zonouz, M. Abdel-Mottaleb, "Identification Using 
+%   Encrypted Biometrics," Computer Analysis of Images and Patterns, 
+%   Springer Berlin Heidelberg, pp. 440-448, 2013.
+% 
+% 
+% (C)	Mohammad Haghighat, University of Miami
+%       haghighat@ieee.org
+%       I WILL APPRECIATE IF YOU CITE OUR PAPER IN YOUR WORK.
+
+
+if (nargin ~= 6)    % Check correct number of arguments
+    error('Use correct number of input arguments!')
+end
+
+if size(img,3) == 3	% % Check if the input image is grayscale
+    img = rgb2gray(img);
+end
+
+img = double(img);
+
+
+%% Filtering
+
+% Filter input image by each Gabor filter
+[u,v] = size(gaborArray);
+gaborResult = cell(u,v);
+for i = 1:u
+    for j = 1:v
+        gaborResult{i,j} = abs(conv2(img,gaborArray{i,j},'same'));        
+    end
+end
+
+
+%% Feature Extraction
+
+% chave � cell e parenteses � a matriz naquela posi��o da c�lula
+% Faz a troca
+[y,x] = size(img);
+for i = 1:u
+    for j = 1:v
+        for a = 1:y
+            for b = 1:x
+                G{a,b}(i,j) = gaborResult{i,j}(a,b);
+            end
+        end
+    end
+end
+ if(u >1) 
+            for a = 1:y % Percorre a c�lula 512x512
+                for b = 1:x
+                    % recebe um valor de magnitude
+                    % Normaliza��o da orienta��o
+                    for i = 1:u % Percorre os valores de escala e orienta��o 2x3
+                        for j = 1:v
+                            % Normaliza��o da orienta��o
+                            %norma = norm(G{a,b}(:,j));
+                            G{a,b}(i,j) = G{a,b}(i,j)/norm(G{a,b}(:,j));
+                        end
+                    end
+                end
+            end
+        end
+        
+        
+        for a = 1:y % Percorre a c�lula 512x512
+            for b = 1:x
+                % recebe um valor de magnitude
+                % Normaliza��o da orienta��o
+                for i = 1:u % Percorre os valores de escala e orienta��o 2x3
+                    for j = 1:v
+                        % Supress�o n�o m�xima da escala
+                       
+                        if G{a,b}(i,j) == max(G{a,b}(i,:))
+                            G{a,b}(i,j) = G{a,b}(i,j); 
+                        else
+                            G{a,b}(i,j) = 0;
+                        end
+                    end
+                end
+            end
+        end
+%% Monta os blocos
+[row,col]=size(G);
+% Valores dos blocos
+aa = 1:N:row;
+bb = 1:K:col;
+[ii,jj] = ndgrid(aa,bb);
+out = arrayfun(@(x,y) G(x:x+N-1,y:y+K-1),ii,jj,'un',0);
+[row,col] = size(out);
+
+%% Som�torios
+n = 1;
+sum1 = zeros(u,v);
+for l = 1:row 
+    for k = 1:col
+        for a = 1:N % Percorre o bloco
+            for b = 1:K
+                % recebe um valor de magnitude
+              
+                        % Supress�o n�o m�xima da escala
+                        if n == (N*K)+1
+                            n = 1;
+                            sum1 = zeros(u,v);
+                        end
+                            temp = cell2mat(out{l,k}(a,b));
+                            sum1 = temp + sum1;                            
+                            out1{l,k} = sum1;
+                            n = n+1;                                 
+            end
+        end
+        % Concatena a matriz e realiza o ultimo som�torio
+       soma = sum(out1{l,k},2);
+       soma2 = sum(soma);
+       out2(l,k) = soma2;
+    end
+end
+
+C = out2(:);
+C = C';
+
+l1norm = norm(C, 1);
+H = C/l1norm;
+featureVector = H;
+%H = reshape(H,row,col);
+%H = mat2gray(H);
+% imshow(H);
+% 
+%  figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
+%  for i = 1:u
+%      for j = 1:v        
+%          subplot(u,v,(i-1)*v+j)   
+%         disp([int2str(i) ',' int2str(j)]);
+%       
+%          imshow(gaborResult{i,j},[]);
+%       
+%      end
+%  end
+
