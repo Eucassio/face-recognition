0534cde6ac66e2c00271cb11b7ee8e895ebdbaca
Eucassio Gonçalves <eucassiojr@gmail.com>
2016-08-18 22:30:37 -0300

    Metricas adicionais

--- gabor/metricas/metricas.py
+++ gabor/metricas/metricas.py
@@ -19,19 +19,37 @@ clf = svm.SVC(kernel='rbf', gamma=0.000030517578125, C = 8.0)
 
 predicted = cross_validation.cross_val_predict(clf, X_train,y_train, cv=6)
 
-precisao = precision_score(y_train, predicted, average='micro')  
-print precisao
+precisaomacro = precision_score(y_train, predicted, average='macro')  
+precisaomicro = precision_score(y_train, predicted, average='micro')  
+precisaoweighted = precision_score(y_train, predicted, average='weighted')  
+
 acuracia = metrics.accuracy_score(y_train, predicted)  
-print acuracia
 con = confusion_matrix(y_train, predicted)
-print con
-f1Score = f1_score(y_train, predicted, average='micro')  
-recall = recall_score(y_train, predicted, average='micro')  
+
+f1Scoremacro = f1_score(y_train, predicted, average='macro')
+f1Scoremicro = f1_score(y_train, predicted, average='micro')  
+f1Scoreweighted = f1_score(y_train, predicted, average='weighted')  
+
+
+recallmacro = recall_score(y_train, predicted, average='macro')  
+recallmicro = recall_score(y_train, predicted, average='micro')
+recallweighted = recall_score(y_train, predicted, average='weighted')
+
 f = open('resultados.txt','wb')
-f.write('precisao: ' + '{}'.format(precisao)+'\n')
+
+f.write('precisao    macro: ' + '{}'.format(precisaomacro)+'\n')
+f.write('precisao    micro: ' + '{}'.format(precisaomicro)+'\n')
+f.write('precisao weighted: ' + '{}'.format(precisaoweighted)+'\n')
+
 f.write('acuracia: ' + '{}'.format(acuracia)+'\n')
-f.write('f1Score:  ' + '{}'.format(f1Score)+'\n')
-f.write('recall:   ' + '{}'.format(recall)+'\n')
+
+f.write('f1Score    macro: ' + '{}'.format(f1Scoremacro)+'\n')
+f.write('f1Score    micro: ' + '{}'.format(f1Scoremicro)+'\n')
+f.write('f1Score weighted: ' + '{}'.format(f1Scoreweighted)+'\n')
+
+f.write('recall    macro: ' + '{}'.format(recallmacro)+'\n')
+f.write('recall    micro: ' + '{}'.format(recallmicro)+'\n')
+f.write('recall weighted: ' + '{}'.format(recallweighted)+'\n')
 
 numpy.savetxt(f,con,fmt="%s")
 f.close()
