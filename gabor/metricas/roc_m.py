349cb1db8ad1dccdb647e122d7e0c39c2a8b49fe
Eucassio Gonçalves <eucassiojr@gmail.com>
2016-10-15 13:50:35 -0300

    .

--- gabor/metricas/roc_m.py
+++ gabor/metricas/roc_m.py
@@ -18,18 +18,20 @@ from sklearn import metrics
 import numpy
 
 
-X, y = load_svmlight_file("/home/eucassio/mestrado/face-recognition/gabor/resultados/processados/03072016/teste.txt")
+X, y = load_svmlight_file("/media/eucassio/dados/facedatabase/vetores de caracteristicas ARFACE/filtro_gabor_entropia_2pi_ARface_48_56_expressoes_0_100_8_4x4_5x8_15x15__concatenado.txt")
 X = X.toarray()
 print len(X)
 yLabels = range(1,101);
 
 
 # Binarize the output
+
 y = label_binarize(y, np.unique(y))
+print y
 n_classes = y.shape[1]
 
 # shuffle and split training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33,
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.30,
                                                     random_state=0)
 
 # Learn to predict each class against the other
@@ -37,70 +39,74 @@ classifier = OneVsRestClassifier(svm.SVC(kernel='rbf', gamma=0.000030517578125, 
 fi2 = classifier.fit(X_train, y_train)
 y_score = fi2.decision_function(X_test)
 
-predicted = fi2.predict(X_test)
-#= cross_validation.cross_val_predict(clf, X_train,y_train, cv=6)
+acuracia = fi2.score(X_test,y_test)
 
+print "acuracia: "
+print(acuracia)
+
+predicted = fi2.predict(X_test)
 
 
 # Compute ROC curve and ROC area for each class
 fpr = dict()
 tpr = dict()
 roc_auc = dict()
-for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-    roc_auc[i] = auc(fpr[i], tpr[i])
+#for i in range(n_classes):
+#   fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+#    roc_auc[i] = auc(fpr[i], tpr[i])
 
 # Compute micro-average ROC curve and ROC area
 fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
 roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
 
-##############################################################################
-# Plot of a ROC curve for a specific class
-#print("Imagem 1")
-#plt.figure()
-#plt.plot(fpr[2], tpr[2], label='ROC curve (area = %0.2f)' % roc_auc[2])
-#plt.plot([0, 1], [0, 1], 'k--')
-#plt.xlim([0.0, 1.0])
-#plt.ylim([0.0, 1.05])
-#plt.xlabel('False Positive Rate')
-#plt.ylabel('True Positive Rate')
-#plt.title('Receiver operating characteristic example')
-#plt.legend(loc="lower right")
-#plt.show()
+
+precisaomacro = precision_score(y_test, predicted, average='macro')  
+precisaomicro = precision_score(y_test, predicted, average='micro')  
+precisaoweighted = precision_score(y_test, predicted, average='weighted')  
+
+acuracia = metrics.accuracy_score(y_test, predicted)  
+#con = confusion_matrix(y_test, predicted)
+
+f1Scoremacro = f1_score(y_test, predicted, average='macro')
+f1Scoremicro = f1_score(y_test, predicted, average='micro')  
+f1Scoreweighted = f1_score(y_test, predicted, average='weighted')  
 
 
-##############################################################################
-# Plot ROC curves for the multiclass problem
+recallmacro = recall_score(y_test, predicted, average='macro')  
+recallmicro = recall_score(y_test, predicted, average='micro')
+recallweighted = recall_score(y_test, predicted, average='weighted')
 
-# Compute macro-average ROC curve and ROC area
+f = open('resultados.txt','wb')
 
-# First aggregate all false positive rates
-all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-print("agregado")
-# Then interpolate all ROC curves at this points
-mean_tpr = np.zeros_like(all_fpr)
-for i in range(n_classes):
-    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+f.write('precisao    macro: ' + '{}'.format(precisaomacro)+'\n')
+f.write('precisao    micro: ' + '{}'.format(precisaomicro)+'\n')
+f.write('precisao weighted: ' + '{}'.format(precisaoweighted)+'\n')
 
-# Finally average it and compute AUC
-mean_tpr /= n_classes
+f.write('acuracia: ' + '{}'.format(acuracia)+'\n')
 
-fpr["macro"] = all_fpr
-tpr["macro"] = mean_tpr
-roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-print("plotando")
-# Plot all ROC curves
+f.write('f1Score    macro: ' + '{}'.format(f1Scoremacro)+'\n')
+f.write('f1Score    micro: ' + '{}'.format(f1Scoremicro)+'\n')
+f.write('f1Score weighted: ' + '{}'.format(f1Scoreweighted)+'\n')
+
+f.write('recall    macro: ' + '{}'.format(recallmacro)+'\n')
+f.write('recall    micro: ' + '{}'.format(recallmicro)+'\n')
+f.write('recall weighted: ' + '{}'.format(recallweighted)+'\n')
+
+f.write('AUC            : ' + '{}'.format(roc_auc["micro"])+'\n')
+
+#numpy.savetxt(f,con,fmt="%s")
+f.close()
+
+
+
+
 plt.figure()
 plt.plot(fpr["micro"], tpr["micro"],
          label='micro-average ROC curve (area = {0:0.2f})'
                ''.format(roc_auc["micro"]),
          linewidth=2)
 
-plt.plot(fpr["macro"], tpr["macro"],
-         label='macro-average ROC curve (area = {0:0.2f})'
-               ''.format(roc_auc["macro"]),
-         linewidth=2)
 
 #for i in range(n_classes):
 #    plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
